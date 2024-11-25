c7bd5602ddfdb98f8e7fceb058ebf85c3923670a
store-x <anipgupta31@gmail.com>
2024-11-25 18:57:35 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,6 +7,6 @@ from .pagination import paginate
 async def is_bot_admin(client: Client, chat_id: int) -> bool:
     try:
         member = await client.get_chat_member(chat_id, client.me.id)
-        return member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR]
+        return member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
     except:
         return False
