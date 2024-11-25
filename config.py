83f7d38a5d7ed81e112471b78c776585e50f6d81
store-x <anipgupta31@gmail.com>
2024-11-25 08:14:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '7003380139:AAG-FAxLg2ODj5f3MwPzYos67tp3EMziSHc')
+    API_ID = int(os.getenv("API_ID", 12380656)) 
+    API_HASH = os.getenv("API_HASH", 'd927c13beaaf5110f25c505b7c071273')  
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://queenxytra:queenxytra@cluster0.ivuxz80.mongodb.net/?retryWrites=true&w=majority")
+    # Razorpay Keys
+    RAZORPAY_API_KEY = os.getenv("RAZORPAY_API_KEY")
+    RAZORPAY_API_SECRET = os.getenv("RAZORPAY_API_SECRET")
+    # General Bot Settings
+    MAX_CHANNELS_PER_PAGE = int(os.getenv("MAX_CHANNELS_PER_PAGE", 5))  # Default is 5 channels per page
+    PAYMENT_TIMEOUT_MINUTES = int(os.getenv("PAYMENT_TIMEOUT_MINUTES", 15))  # Default is 15 minutes
+    # Admin Settings
+    ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", '22,3').split(",")))  # Comma-separated admin user ID
+    
+    
+
