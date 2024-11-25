f759d1451d353866c730be5177d9dc9d25de78fb
store-x <anipgupta31@gmail.com>
2024-11-25 19:05:34 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,10 +3,12 @@ from pyrogram.errors import UserNotParticipant
 from pyrogram.enums import ChatMemberStatus
 
 from .pagination import paginate 
+from PayBot import BOT_ID
 
 async def is_bot_admin(client: Client, chat_id: int) -> bool:
     try:
-        member = await client.get_chat_member(chat_id, client.me.id)
+        member = await client.get_chat_member(chat_id, BOT_ID)
         return member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
-    except:
+    except Exception as e:
+        print(e)
         return False
