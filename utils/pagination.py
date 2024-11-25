dc51a707d0c2224895a4d000321fbc22f7067545
store-x <anipgupta31@gmail.com>
2024-11-25 22:43:11 +0530

    Update pagination.py

--- utils/pagination.py
+++ utils/pagination.py
@@ -7,7 +7,7 @@ async def paginate(ch_data: List[dict], max_btn_per_page: int, current_page: int
     start_index = (current_page - 1) * max_btn_per_page
     end_index = min(start_index + max_btn_per_page, total_channels)
     buttons = [
-        InlineKeyboardButton(channel['name'], callback_data=f"{cb_var}_{channel['channel_id']}")
+        [InlineKeyboardButton(channel['name'], callback_data=f"{cb_var}_{channel['channel_id']}")]
         for channel in ch_data[start_index:end_index]
     ]
     
@@ -17,4 +17,4 @@ async def paginate(ch_data: List[dict], max_btn_per_page: int, current_page: int
     if current_page < total_pages:
         navigation_buttons.append(InlineKeyboardButton("Next", callback_data=f"{cb_var}_next_{current_page + 1}"))
     
-    return InlineKeyboardMarkup([buttons] + [navigation_buttons] if navigation_buttons else [buttons])
+    return InlineKeyboardMarkup(buttons + [navigation_buttons] if navigation_buttons else buttons)
