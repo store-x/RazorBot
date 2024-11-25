9649b92cd6337f51224a533613a138b67f03a451
store-x <anipgupta31@gmail.com>
2024-11-25 11:45:18 +0530

    Update __init__.py

--- PayBot/__init__.py
+++ PayBot/__init__.py
@@ -4,7 +4,7 @@ import sys
 import logging
 import logging.handlers as handlers
 from motor import motor_asyncio
-from pyrogram import Client
+from pyromod import Client
 from config import Settings
 
 
