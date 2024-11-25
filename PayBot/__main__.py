95ce172ff065298e7eaa9e55975648beb102f58e
store-x <anipgupta31@gmail.com>
2024-11-25 09:10:28 +0530

    Update __main__.py

--- PayBot/__main__.py
+++ PayBot/__main__.py
@@ -2,14 +2,14 @@
 import asyncio
 import importlib
 from pyrogram import idle
-from Modules.handlers import ALL_MODULES
-from Modules import bot 
+from PayBot.handlers import ALL_MODULES
+from PayBot import bot 
 
 loop = asyncio.get_event_loop()
 
 async def cbot_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("Modules.handlers." + all_module)
+        importlib.import_module("PayBot.handlers." + all_module)
     print(str(ALL_MODULES))
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎lly 𝗌𝗍𝖺𝗋𝗍")
     await idle()
